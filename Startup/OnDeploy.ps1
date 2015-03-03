9990f0abab628b8539fce72ae8e1796b0ef3a003
altrive <altrive@outlook.com>
2015-03-04 03:12:24 +0900

    Set startupTimeLimit to 30 sec.

--- Startup/OnDeploy.ps1
+++ Startup/OnDeploy.ps1
@@ -11,21 +11,17 @@ $host.UI.WriteLine('$env:DEPLOYMENT_TARGET: {0}' -f $env:DEPLOYMENT_TARGET)
 
 #a temporary folder that can be used to store artifacts for the current build. This folder is deleted after the cmd is run.
 $host.UI.WriteLine('$env:DEPLOYMENT_TEMP: {0}' -f $env:DEPLOYMENT_TEMP)
-
-$host.UI.WriteLine('$env:deployment_branch: {0}' -f $env:deployment_branch)
-
+$host.UI.WriteLine('$env:deployment_branch: {0}' -f $env:deployment_branch
 $host.UI.WriteLine('$env:SCM_COMMIT_ID: {0}' -f $env:SCM_COMMIT_ID)
-
 $host.UI.WriteLine('$env:gitBucketVersion: {0}' -f $env:gitBucketVersion)
-
-#WellKnownEnvironmentVariables
-#https://github.com/projectkudu/kudu/blob/bb1e4bd69ccbd86268888aec88316f13f50b7108/Kudu.Core/Deployment/WellKnownEnvironmentVariables.cs
 
 $env:GITBUCKET_HOME = "$env:HOME/GitBucket"
 $env:CONTEXT_PATH = "/"
 
 #Download GitBuckets
-$source = 'https://github.com/takezoe/gitbucket/releases/download/2.8/gitbucket.war'
+$source = 'https://github.com/takezoe/gitbucket/releases/download/{0}/gitbucket.war' -f $env:gitBucketVersion
+
+#TODO: Check version
 if(!(Test-Path "$env:WEBROOT_PATH\webapps"))
 {
     New-Item "$env:WEBROOT_PATH\webapps" -ItemType Directory -ErrorAction Ignore > $null
@@ -42,7 +38,9 @@ $xml = [xml]@'
     <handlers>
       <add name="httpPlatformHandlerMain" modules="httpPlatformHandler" path="*" verb="*" resourceType="Unspecified" />
     </handlers>
-    <httpPlatform processPath="{{PROCESS_PATH}}" arguments="{{ARGUMENTS}}">
+    <httpPlatform processPath="{{PROCESS_PATH}}" 
+                  arguments="{{ARGUMENTS}}"
+                  startupTimeLimit="30">
       <environmentVariables>
         <environmentVariable name="GITBUCKET_HOME" value="{{GITBUCKET_HOME}}" />
       </environmentVariables>
@@ -51,8 +49,8 @@ $xml = [xml]@'
 </configuration>
 '@.Replace("{{GITBUCKET_HOME}}", (Join-Path $env:Home ".gitbucket"))
 
-$xml.configuration."system.webServer".httpPlatform.processPath = "$env:JAVA_HOME\bin\java.exe"
-$xml.configuration."system.webServer".httpPlatform.arguments   = [Environment]::ExpandEnvironmentVariables("$env:AZURE_JETTY9_CMDLINE")
+$xml.configuration."system.webServer".httpPlatform.processPath = '%JAVA_HOME%\bin\java.exe'
+$xml.configuration."system.webServer".httpPlatform.arguments   = $env:AZURE_JETTY9_CMDLINE
 
 Add-Type -AssemblyName System.Xml.Linq
 $xmlText = [System.Xml.Linq.XElement]::Parse($xml.OuterXml).ToString()
